a95646d6ae7c0a8e42aa590a739e083d5e1f0ed3
coolreader18 <33094578+coolreader18@users.noreply.github.com>
2017-11-14 07:27:36 -0600

    some js

---
+++ docs/import.js
@@ -0,0 +1,11 @@
+function checkcalc(cstring) {
+  if (cstring.indexOf("{") == 0 && cstring.split("").reverse().join("").indexOf("}") == 0) {
+
+  }
+  return false;
+}
+var calculator = {
+  variables: [],
+  equation:
+};
+var hash = window.location.hash.split("#")[1];
--- docs/import.md
+++ docs/import.md
@@ -1,10 +1,6 @@
 ---
 permalink: /import/
 ---
-<script>
-var calculator = {
-  variables: [],
-  equation: 
-};
-var hash = window.location.hash.split("#")[1];
-</script>
+<script src="/import.js">
+
+<div id=calculator></div>
