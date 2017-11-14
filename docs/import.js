35562cb0678d0cfa030def6a662b6c24ad3d1f7d
coolreader18 <33094578+coolreader18@users.noreply.github.com>
2017-11-14 12:25:21 -0600

    js

--- docs/import.js
+++ docs/import.js
@@ -1,11 +1,11 @@
-function checkcalc(cstring) {
-  if (cstring.indexOf("{") == 0 && cstring.split("").reverse().join("").indexOf("}") == 0) {
-
-  }
-  return false;
-}
 var calculator = {
   variables: [],
   equation:
 };
+function openCalc(calc) {
+  calculator = JSON.parse(calc);
+}
 var hash = window.location.hash.split("#")[1];
+if(hash) {
+  openCalc(hash);
+}
