a3a3f4d4de1bae3cc1a2b846a567fb99d1b0122e
coolreader18 <33094578+coolreader18@users.noreply.github.com>
2017-11-15 15:04:50 -0600

    BIG THING

--- docs/import.js
+++ docs/import.js
@@ -1,11 +1,31 @@
 var calculator = {
-  variables: [],
-  equation:
+  data: {
+    varlist: ["a","b","c"],
+    equation: "b*a*c";
+  },
+  definevars: function() {
+    var varlist = calculator.data.varlist;
+    for (i = 0; i < varlist.length; i++) {
+      calculator.vars[varlist[i]] = undefined;
+    }
+  },
+  import: function(cstring) {
+  	calculator.data = JSON.parse(cstring);
+  },
+  assignvars: function() {
+    var varlist = calculator.data.varlist;
+    var args = Array.from(arguments);
+    for (var i = 0; i < varlist.length; i++) {
+      calculator.vars.varvals[varlist[i]] = args[i];
+    }
+  },
+  varvals: {},
+  calculate: function() {
+    assignvars(arguments);
+    return math.eval(calculator.data.equation, calculator.varvals);
+  }
 };
-function openCalc(calc) {
-  calculator = JSON.parse(calc);
-}
-var hash = window.location.hash.split("#")[1];
-if(hash) {
-  openCalc(hash);
-}
+
+
+calculator.vars.define();
+document.getElementById("thing").innerHTML = calculator.calculate(1,2,3);
