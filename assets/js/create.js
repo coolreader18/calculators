9c36d4ac3b5cea553a0b0b72ef7682d0787eda57
coolreader18 <33094578+coolreader18@users.noreply.github.com>
2017-11-21 12:23:31 -0600

    better explanations and formatting

--- assets/js/create.js
+++ assets/js/create.js
@@ -2,11 +2,15 @@ function oncrDivLoad() {
   crdiv = document.getElementById("create");
   varinput = crdiv.appendChild(document.createElement("input"));
   varinput.setAttribute("type","text");
+  var.setAttribute("placeholder","eg. a,b,c");
   var label = crdiv.insertBefore(document.createElement("label"), varinput);
-  label.innerHTML = "Variable names here, separated by a comma: ";
+  label.innerHTML = "Variable names: ";
   crdiv.appendChild(document.createElement("br"));
   equation = crdiv.appendChild(document.createElement("input"));
   equation.setAttribute("type","text");
+  equation.setAttribute("placeholder","eg. (-b + sqrt((b^2)-4*a*c))/2a")
+  label = crdiv.insertBefore(document.createElement("label"), equation);
+  label.innerHTML = "Equation: "
   crdiv.appendChild(document.createElement("br"));
   var button = crdiv.appendChild(document.createElement("button"));
   button.className = "btn";
